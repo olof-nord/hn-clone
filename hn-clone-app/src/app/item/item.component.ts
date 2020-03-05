1d902f8b786e51950c1bb2b6141b41e2e76eca9c
Olof Nord <olof.nord@tutanota.com>
2020-03-05 22:22:21 +0100

    Item display improvements
    Mimic HN links for items
    Add HN favicon

--- hn-clone-app/src/app/item/item.component.ts
+++ hn-clone-app/src/app/item/item.component.ts
@@ -14,8 +14,31 @@ export class ItemComponent implements OnInit {
   @Input()
   index: number;
 
+  private baseURL = 'https://news.ycombinator.com/';
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  getHostname(): string {
+    const url: URL = new URL(this.item?.url);
+
+    const tld = url.hostname.split('.').slice(-1)[0];
+    const domain = url.hostname.split('.').slice(-2)[0];
+
+    return domain + '.' + tld;
+  }
+
+  getOtherItemsURI(): string {
+    return this.baseURL + 'from?site=' + this.getHostname();
+  }
+
+  getUserURI(): string {
+    return this.baseURL + 'user?id=' + this.item?.by;
+  }
+
+  getHideURI(): string {
+    return this.baseURL + 'hide?id=' + this.item?.id + '&goto=news';
+  }
 
 }
